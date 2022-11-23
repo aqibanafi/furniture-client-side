d9d2355d36ad82a54e115f444782edeeda067af5
unknown <you@example.com>
2022-11-24 02:59:15 +0600

    Dashboard & Logout Implementation

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../../assets/logo/logo.png'
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const Header = () => {
+
+    //Import Auth Context
+    const { user, logOut } = useContext(AuthContext)
+
+    //Navigate
+    const navigate = useNavigate()
+
+    //Handle Log Out 
+    const handleLogOut = () => {
+        logOut()
+            .then(() => {
+                toast.error("You Have Logged Out")
+                navigate('/')
+                localStorage.removeItem('superkitch');
+            })
+            .catch(error => console.error(error))
+    }
+
     const menuItems =
         <>
             <div className='flex gap-10'>
-                <Link><li>Home</li></Link>
-                <Link><li>Blog</li></Link>
-                <Link><li>Contact</li></Link>
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/blog'><li>Blog</li></Link>
+                <Link to='/contact'><li>Contact</li></Link>
+                {
+                    user?.email &&
+                    <Link to='/dashbaord'><li>Dashbaord</li></Link>
+                }
             </div>
         </>
     return (
@@ -23,6 +47,8 @@ const Header = () => {
                             {menuItems}
                         </ul>
                     </div>
+
+
                     <div className='flex items-center gap-5'>
                         <Link to='/'><img className='hidden lg:block' src={Logo} alt="" /></Link>
                         <Link to='/' className="text-primary font-bold text-xl">The Personal</Link>
@@ -34,8 +60,25 @@ const Header = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <button className='btn btn-secondary'>Login</button>
+                    <div>
+                        {
+                            user?.email ?
+                                <></>
+                                :
+                                <Link to='/login'><button className='btn btn-secondary'>Login</button></Link>
+                        }
+                    </div>
+                    <div>
+                        {
+                            user?.email &&
+                            <div>
+                                <Link onClick={handleLogOut} className='btn btn-secondary'>Logout</Link>
+                            </div>
+                        }
+                    </div>
                 </div>
+
+
             </div>
         </div>
     );
