7c8159476a3deaa6d5921f64b4b6fe3c02e86e57
unknown <you@example.com>
2022-11-24 02:38:42 +0600

    Login Function Implementation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,17 +1,61 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import { AuthContext } from '../../context/AuthProvider/AuthProvider';
+import { GoogleAuthProvider } from 'firebase/auth';
+import toast from 'react-hot-toast';
 
 const Login = () => {
 
     //Import Auth Info
     const { signIn, resetPassword, googleProviderLogin } = useContext(AuthContext)
 
+    //Email State
+    const [getEmail, setGetEmail] = useState(null)
+
+    //Navigate and Location
+    const navigate = useNavigate()
+    const location = useLocation()
+    const from = location.state?.from?.pathname || '/';
+
     //Form handle Functions
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
-        
+        const email = data.email;
+        setGetEmail(data.email)
+        const password = data.password;
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => console.error(error))
+
+    }
+
+    //Handle Google Login
+    const googleProvider = new GoogleAuthProvider()
+
+    const handleGoogleSignUp = () => {
+        googleProviderLogin(googleProvider)
+            .then(result => {
+                toast.success("You Have Successfully Logged in")
+                const user = result.user;
+                navigate(from, { replace: true })
+                console.log(user);
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
+    //Handle Reset Password
+    const handleResetPassword = () => {
+        resetPassword(getEmail)
+            .then(() => {
+                toast.success("Password Reset Link Sent")
+            })
+            .catch(error => console.error(error))
     }
 
     return (
@@ -22,7 +66,7 @@ const Login = () => {
                     <Link to='/registration' rel="noopener noreferrer" className="focus:underline hover:underline ml-2">registration here</Link>
                 </p>
                 <div className="my-6 space-y-4">
-                    <button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400">
+                    <button onClick={handleGoogleSignUp} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                             <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                         </svg>
@@ -41,10 +85,13 @@ const Login = () => {
                         <input {...register("email", { required: true })} type="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
                         {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
                     </div>
-                    <div className='w-full mb-5'>
+                    <div className='w-full mb-2'>
                         <label for="email" className="block text-sm mb-1">Password</label>
                         <input {...register("password", { required: true })} type="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
                         {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                    </div>
+                    <div>
+                        <Link onClick={handleResetPassword}><p className='text-gray-500 hover:text-amber-600'><small>Forget Password?</small></p></Link>
                     </div>
                     <div className='mt-10 flex justify-center'>
                         <input className='bg-accent text-primary font-bold px-20 py-4' type="submit" />
--- src/Pages/Registration/Registration.js
+++ src/Pages/Registration/Registration.js
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { useForm } from "react-hook-form";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 import placeholder from '../../assets/imageplaceholder/placeholder2.png'
+import { GoogleAuthProvider } from 'firebase/auth';
+import toast from 'react-hot-toast';
 
 const Registration = () => {
 
@@ -16,6 +18,9 @@ const Registration = () => {
     const [email, setEmail] = useState(null);
     const [profileType, setProfileType] = useState(null);
     const [image, setImage] = useState(null)
+
+    //Navigate and Location
+    const navigate = useNavigate()
 
 
     //Form handle Functions
@@ -40,8 +45,50 @@ const Registration = () => {
             .then(res => res.json())
             .then(data => setImage(data.data.display_url))
 
-        //Use Post Method To Store Image into Image BB
+        //Create User
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => console.error(error))
+        handleUpdateUserProfile(name, image)
+        const userInfo = {
+            name: name,
+            email: email,
+            location: location,
+            image: image,
+            role: profileType
+        }
+        navigate('/')
 
+    }
+
+    //Handle Google Login
+    const googleProvider = new GoogleAuthProvider()
+
+    const handleGoogleSignUp = () => {
+        googleProviderLogin(googleProvider)
+            .then(result => {
+                toast.success("You Have Successfully Logged in")
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
+
+    //Handle Update User Profile 
+    const handleUpdateUserProfile = (name, photoURL) => {
+        const profile = {
+            displayName: name,
+            photoURL: photoURL
+        }
+        updateUserProfile(profile)
+            .then(() => { })
+            .catch(error => console.error(error))
     }
 
     //Image Placeholder and Show Image Preview 
@@ -75,7 +122,7 @@ const Registration = () => {
                     <Link to='/login' rel="noopener noreferrer" className="focus:underline hover:underline ml-2">login here</Link>
                 </p>
                 <div className="my-6 space-y-4">
-                    <button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400">
+                    <button onClick={handleGoogleSignUp} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                             <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                         </svg>
