acf80bb3d5464a2e020a8014ca3472fb90835f95
unknown <you@example.com>
2022-11-23 22:51:43 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#062037",
+        
+"secondary": "#68ff93",
+        
+"accent": "#f927b0",
+        
+"neutral": "#272A35",
+        
+"base-100": "#E6E5EB",
+        
+"info": "#3967DB",
+        
+"success": "#1E9956",
+        
+"warning": "#DE9012",
+        
+"error": "#ED5250",
+        },
+      },
+    ],
+  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
